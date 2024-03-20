44cd2ef3c9502cfe7afe8f37c1db422147b78f07
rohan <rohanthakur89768@gmail.com>
2024-03-20 11:17:11 +0530

    added home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,33 @@
-
+import logo from 'Assets/images/book.png';
 
 export default function Home(){
     return(
-        <>home</>
+    
+            <div className="flex flex-col items-center justify-center gap-20 h-[100vh]">
+
+                <div className='h-40 w-40 '>
+                    <img
+                       className='w-full h-full rounded-full'
+                       alt="logo"
+                       src={logo}
+                    />
+                </div>
+
+                <div className='flex  justify-around  items-center gap-4 text-3xl'>
+                    <div className='w-2/4 text-center font-semibold basis-1/2'>
+                        <h1 className='text-white text-5xl tracking-widest leading-normal'>BookShelf <br/>
+                        <span className='text-warning'>Ur Personal Libarary</span>
+                       </h1>
+                    </div>
+                    <div>
+                        <button className='btn btn-primary rounded-md px-5 py-2 text-xl '>Register</button>
+                        <button className='btn btn-warning mx-3 rounded-md px-5 py-2 text-xl '>Login</button>
+                    </div>
+
+                </div>
+
+            </div>
+        
     );
     
 }
